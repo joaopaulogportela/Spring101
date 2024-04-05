3245377a2911af215af6f4f439654aaf90635c08
joaopauloportela <joaopauloportela@hotmail.com>
2024-04-05 17:14:43 +0100

    03 - setter injection (basicamente ao inves de usar o construtor com @Autowired para injetar dependencias, usa-se um método qualuqer (normalmente com nome de set))

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach coach){
+    public void setCoach(Coach coach){
         this.myCoach = coach;
     }
 
