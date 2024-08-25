54b588f3309c7d4cac69fbbab74a6414dec4f600
joaopauloportela <joaopauloportela@hotmail.com>
2024-08-25 11:57:50 +0100

    6.1- @Lazy annotation can be default applied to all components without being explicit tagged. in application.properties if you add the command "spring.main.lazy-initialization = true" it will be applied to all components in the project.

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Lazy
 public class DemoController {
     // define private field for dependency
     private Coach myCoach;
